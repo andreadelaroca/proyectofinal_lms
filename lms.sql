e4acf19739a9edb4e7eb63830d08e3326a7532ad
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-02 09:34:21 -0600

    feat(): Añadí comando set single_user with rollback immediate para asegurar la eliminación de la BD

--- lms.sql
+++ lms.sql
@@ -5,6 +5,7 @@ GO
 
 IF EXISTS(SELECT * FROM sys.databases WHERE NAME = 'lmsDB')
 	BEGIN
+		ALTER DATABASE lmsDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 		DROP DATABASE lmsDB
 	END
 GO
@@ -15,89 +16,21 @@ GO
 USE lmsDB
 GO
 
+--Módulo 1: Gestión de Roles y Permisos
 CREATE SCHEMA GestionRolesPermisos
-GO
-
-CREATE SCHEMA GestionIdentidadAcad
-GO
-
-CREATE SCHEMA GestionTutores
-GO
-
---M�dulo 4: Gesti�n de Eventos y Sesiones Acad�micas
-CREATE SCHEMA GestionEventos
-GO
-
---Tabla 4.1: Sesiones
-CREATE TABLE GestionEventos.Sesiones (
-	id_sesion INT IDENTITY(1,1) CONSTRAINT PK_id_sesion PRIMARY KEY
-	, id_tutor INT CONSTRAINT FK_id_tutor FOREIGN KEY REFERENCES GestionTutores.Tutores(id_tutor)
-	, id_materia INT CONSTRAINT FK_id_materia FOREIGN KEY REFERENCES GestionIdentidadCad.Materias(id_materia)
-	, fecha DATE NOT NULL
-	, hora_inicio DATETIME NOT NULL
-	, hora_fin DATETIME NOT NULL
-	, ubicacion NVARCHAR(60) NOT NULL
-	, cupo_max INT NOT NULL CONSTRAINT CK_cupo_max_val CHECK(cupo_max > 0)
-	, created_at DATETIME CONSTRAINT DF_created_at_ses DEFAULT GETDATE()
-	, updated_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at >= updated_at)
-	, deleted_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at > deleted_at)
-)
-GO
-
---Tabla 4.2: Inscripciones
-CREATE TABLE GestionEventos.Inscripciones (
-	id_inscripcion INT IDENTITY(1,1) CONSTRAINT PK_id_inscripcion PRIMARY KEY
-	, id_estudiante INT CONSTRAINT FK_id_estudiante FOREIGN KEY REFERENCES GestionTutores.Tutores(id_tutor)
-	, id_sesion INT CONSTRAINT FK_id_sesion FOREIGN KEY REFERENCES GestionEventos.Sesiones(id_sesion)
-	, fecha_inscripcion DATE CONSTRAINT DF_fecha_inscripcion DEFAULT GETDATE()
-	, estado BIT CONSTRAINT DF_estado DEFAULT 0
-	, created_at DATETIME CONSTRAINT DF_created_at_ses DEFAULT GETDATE()
-	, updated_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at >= updated_at)
-	, deleted_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at > deleted_at)
-)
-GO
-
---Tabla 4.3: Asistencias
-CREATE TABLE GestionEventos.Asistencias (
-	id_asistencia INT IDENTITY(1,1) CONSTRAINT PK_id_inscripcion PRIMARY KEY
-	, id_inscripcion INT CONSTRAINT FK_id_inscripcion FOREIGN KEY REFERENCES GestionEventos.Inscripciones(id_inscripcion)
-	, estatus_asistencia NVARCHAR(30) CONSTRAINT CK_estatus_asistencia CHECK(estatus_asistencia IN ('Presente', 'Tarde', 'Falta justificada', 'Falta injustificada'))
-	, participacion TINYINT CONSTRAINT CK_participacion CHECK(participacion BETWEEN 0 AND 5)
-	, fecha_registro DATE NOT NULL
-	, created_at DATETIME CONSTRAINT DF_created_at_ses DEFAULT GETDATE()
-	, updated_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at >= updated_at)
-	, deleted_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at > deleted_at)
-)
-GO
-
---Tabla 4.4: FeedbackEvaluaciones
-CREATE TABLE GestionEventos.FeedbackEvaluaciones (
-	id_feedback INT IDENTITY(1,1) CONSTRAINT PK_id_feedback PRIMARY KEY
-	, id_inscripcion INT FOREIGN KEY REFERENCES GestionEventos.Inscripciones(id_inscripcion)
-	, comentario NVARCHAR(255) NOT NULL
-	, puntuacion_tutor TINYINT CONSTRAINT CK_participacion_tutor CHECK(puntuacion_tutor BETWEEN 0 AND 5)
-)
-GO
-CREATE SCHEMA GestionEventosSesiones
 GO
 
 -- Modulo 1: GestionRolesPermisos
 
+-- Tabla 1.1: Roles
 CREATE TABLE GestionRolesPermisos.Roles (
-    id_rol INT IDENTITY(1,1),
-    nombre VARCHAR(50) 
-	NOT NULL,
-    descripcion VARCHAR(255),
-
-    createdAt DATETIME 
-	DEFAULT GETDATE(),
-    updatedAt DATETIME 
-	NULL,
-    deletedAt DATETIME 
-	NULL,
-    
-    CONSTRAINT PK_Roles 
-	PRIMARY KEY (id_rol),
+    id_rol INT IDENTITY(1,1)
+	, nombre NVARCHAR(50) NOT NULL
+	, descripcion NVARCHAR(255)
+    , created_at DATETIME DEFAULT GETDATE()
+	, updated_at DATETIME NULL
+	, deleted_at DATETIME NULL
+    , CONSTRAINT PK_roles PRIMARY KEY (id_rol),
 )
 GO
 
@@ -202,3 +135,67 @@ CREATE TABLE GestionRolesPermisos.Auditoria (
 	REFERENCES AuditoriaHistorial(id_audit)
 )
 GO
+
+CREATE SCHEMA GestionIdentidadAcad
+GO
+
+CREATE SCHEMA GestionTutores
+GO
+
+--Módulo 4: Gesti�n de Eventos y Sesiones Acad�micas
+CREATE SCHEMA GestionEventos
+GO
+
+--Tabla 4.1: Sesiones
+CREATE TABLE GestionEventos.Sesiones (
+	id_sesion INT IDENTITY(1,1) CONSTRAINT PK_id_sesion PRIMARY KEY
+	, id_tutor INT CONSTRAINT FK_id_tutor FOREIGN KEY REFERENCES GestionTutores.Tutores(id_tutor)
+	, id_materia INT CONSTRAINT FK_id_materia FOREIGN KEY REFERENCES GestionIdentidadCad.Materias(id_materia)
+	, fecha DATE NOT NULL
+	, hora_inicio DATETIME NOT NULL
+	, hora_fin DATETIME NOT NULL
+	, ubicacion NVARCHAR(60) NOT NULL
+	, cupo_max INT NOT NULL CONSTRAINT CK_cupo_max_val CHECK(cupo_max > 0)
+	, created_at DATETIME CONSTRAINT DF_created_at_ses DEFAULT GETDATE()
+	, updated_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at >= updated_at)
+	, deleted_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at > deleted_at)
+)
+GO
+
+--Tabla 4.2: Inscripciones
+CREATE TABLE GestionEventos.Inscripciones (
+	id_inscripcion INT IDENTITY(1,1) CONSTRAINT PK_id_inscripcion PRIMARY KEY
+	, id_estudiante INT CONSTRAINT FK_id_estudiante FOREIGN KEY REFERENCES GestionTutores.Tutores(id_tutor)
+	, id_sesion INT CONSTRAINT FK_id_sesion FOREIGN KEY REFERENCES GestionEventos.Sesiones(id_sesion)
+	, fecha_inscripcion DATE CONSTRAINT DF_fecha_inscripcion DEFAULT GETDATE()
+	, estado BIT CONSTRAINT DF_estado DEFAULT 0
+	, created_at DATETIME CONSTRAINT DF_created_at_ses DEFAULT GETDATE()
+	, updated_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at >= updated_at)
+	, deleted_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at > deleted_at)
+)
+GO
+
+--Tabla 4.3: Asistencias
+CREATE TABLE GestionEventos.Asistencias (
+	id_asistencia INT IDENTITY(1,1) CONSTRAINT PK_id_inscripcion PRIMARY KEY
+	, id_inscripcion INT CONSTRAINT FK_id_inscripcion FOREIGN KEY REFERENCES GestionEventos.Inscripciones(id_inscripcion)
+	, estatus_asistencia NVARCHAR(30) CONSTRAINT CK_estatus_asistencia CHECK(estatus_asistencia IN ('Presente', 'Tarde', 'Falta justificada', 'Falta injustificada'))
+	, participacion TINYINT CONSTRAINT CK_participacion CHECK(participacion BETWEEN 0 AND 5)
+	, fecha_registro DATE NOT NULL
+	, created_at DATETIME CONSTRAINT DF_created_at_ses DEFAULT GETDATE()
+	, updated_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at >= updated_at)
+	, deleted_at DATETIME NULL DEFAULT GETDATE() CHECK(created_at > deleted_at)
+)
+GO
+
+--Tabla 4.4: FeedbackEvaluaciones
+CREATE TABLE GestionEventos.FeedbackEvaluaciones (
+	id_feedback INT IDENTITY(1,1) CONSTRAINT PK_id_feedback PRIMARY KEY
+	, id_inscripcion INT FOREIGN KEY REFERENCES GestionEventos.Inscripciones(id_inscripcion)
+	, comentario NVARCHAR(255) NOT NULL
+	, puntuacion_tutor TINYINT CONSTRAINT CK_participacion_tutor CHECK(puntuacion_tutor BETWEEN 0 AND 5)
+)
+GO
+CREATE SCHEMA GestionEventosSesiones
+GO
+
