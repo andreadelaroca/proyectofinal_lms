8828ab67dc7a015d07e94a41c168854fec3af7f5
KDCar15 <caaguirre@uamv.edu.ni>
2026-06-02 08:35:05 -0600

    Añadidas columnas updatedat, createdat y deletedat

--- lms.sql
+++ lms.sql
@@ -31,8 +31,16 @@ GO
 
 CREATE TABLE GestionRolesPermisos.Roles (
     id_rol INT IDENTITY(1,1),
-    nombre VARCHAR(50) NOT NULL,
+    nombre VARCHAR(50) 
+	NOT NULL,
     descripcion VARCHAR(255),
+
+    createdAt DATETIME 
+	DEFAULT GETDATE(),
+    updatedAt DATETIME 
+	NULL,
+    deletedAt DATETIME 
+	NULL,
     
     CONSTRAINT PK_Roles 
 	PRIMARY KEY (id_rol),
@@ -41,8 +49,16 @@ GO
 
 -- Tabla de los permisos que tiene cada rol
 CREATE TABLE GestionRolesPermisos.PermisosRoles (
-    id_rol INT NOT NULL,
-    id_permiso INT NOT NULL,
+    id_rol INT 
+	NOT NULL,
+    id_permiso INT 
+	NOT NULL,
+    createdAt DATETIME 
+	DEFAULT GETDATE(),
+    updatedAt DATETIME 
+	NULL,
+    deletedAt DATETIME 
+	NULL,
     
     CONSTRAINT PK_PermisosRoles 
 	PRIMARY KEY (id_rol, id_permiso),
@@ -58,8 +74,15 @@ GO
 -- Tabla de permisos y operaciones disponibles
 CREATE TABLE GestionRolesPermisos.DetallesPermisos (
     id_permiso INT IDENTITY(1,1),
-    nombre VARCHAR(50) NOT NULL,
+    nombre VARCHAR(50) 
+	NOT NULL,
     descripcion VARCHAR(255),
+    createdAt DATETIME 
+	DEFAULT GETDATE(),
+    updatedAt DATETIME 
+	NULL,
+    deletedAt DATETIME 
+	NULL,
 
     CONSTRAINT PK_DetallesPermisos
 	PRIMARY KEY (id_permiso)
@@ -70,10 +93,15 @@ GO
 -- Bitacora de accesos e intentos de inicio de sesión
 CREATE TABLE GestionRolesPermisos.RegistrosAcceso (
     id_log INT IDENTITY(1,1),
-    id_usuario INT NOT NULL,
-    estado BIT NOT NULL,
-    ip VARCHAR(45) NOT NULL,
-    hora DATETIME NOT NULL,
+    id_usuario INT 
+	NOT NULL,
+    estado BIT 
+	NOT NULL,
+    ip VARCHAR(45)
+	NOT NULL,
+    hora DATETIME
+	NOT NULL
+	DEFAULT GETDATE(),
 
     CONSTRAINT PK_RegistrosAcceso 
 	PRIMARY KEY (id_log),
@@ -88,7 +116,8 @@ GO
 -- Tabla general del historial de auditoría
 CREATE TABLE GestionRolesPermisos.AuditoriaHistorial (
     id_audit INT IDENTITY(1,1),
-    id_usuario INT NOT NULL,
+    id_usuario INT
+	NOT NULL,
 
     CONSTRAINT PK_AuditoriaHistorial
 	PRIMARY KEY (id_audit),
@@ -100,14 +129,19 @@ GO
 
 -- Detalle técnico del "antes" y "después" de cada cambio en los datos
 CREATE TABLE GestionRolesPermisos.Auditoria (
-    id_audit INT NOT NULL,
-    tabla VARCHAR(50) NOT NULL,
-    columna VARCHAR(50) NOT NULL,
+    id_audit INT 
+	NOT NULL,
+    tabla VARCHAR(50) 
+	NOT NULL,
+    columna VARCHAR(50) 
+	NOT NULL,
     tupla INT NOT NULL,
-    id_operacion INT NOT NULL,
+    id_operacion INT 
+	NOT NULL,
     estado_anterior VARCHAR(MAX),
     estado_actual VARCHAR(MAX),
-    fecha DATETIME NOT NULL,
+    fecha DATETIME 
+	NOT NULL,
     
     CONSTRAINT FK_Auditoria_Historial
 	FOREIGN KEY (id_audit)
