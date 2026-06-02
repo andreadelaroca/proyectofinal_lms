04540855298b4e64d8b572c3ebca01af52cfa83f
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-02 07:54:34 -0600

    feat(): Creación de BD y Schemas

--- lms.sql
+++ lms.sql
@@ -1,1 +1,28 @@
 --Inicio de repositorio
+
+USE master
+GO
+
+IF EXISTS(SELECT * FROM sys.databases WHERE NAME = 'lmsDB')
+	BEGIN
+		DROP DATABASE lmsDB
+	END
+GO
+
+CREATE DATABASE lmsDB
+GO
+
+USE lmsDB
+GO
+
+CREATE SCHEMA GestionRolesPermisos
+GO
+
+CREATE SCHEMA GestionIdentidadAcad
+GO
+
+CREATE SCHEMA GestionTutores
+GO
+
+CREATE SCHEMA GestionEventosSesiones
+GO
