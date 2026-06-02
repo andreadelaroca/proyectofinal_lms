4a2fa16654cd0f3c9b40f3e8cd6b41a22c124417
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-02 08:06:32 -0600

    feat(): Creación de tabla Sesiones del Módulo 4

--- lms.sql
+++ lms.sql
@@ -1,5 +1,5 @@
 --Inicio de repositorio
-
+	
 USE master
 GO
 
@@ -24,5 +24,19 @@ GO
 CREATE SCHEMA GestionTutores
 GO
 
+--Módulo 4: Gestión de Eventos y Sesiones Académicas
 CREATE SCHEMA GestionEventosSesiones
+GO
+
+--Tabla 4.1: Sesiones
+CREATE TABLE Sesiones (
+	id_sesion INT IDENTITY(1,1) CONSTRAINT PK_id_sesion PRIMARY KEY
+	, id_tutor INT FK_id_tutor CONSTRAINT FOREIGN KEY REFERENCES Tutores(id_tutor)
+	, id_materia INT FK_id_materia CONSTRAINT FOREIGN KEY REFERENCES Materias(id_materia)
+	, fecha DATE NOT NULL
+	, hora_inicio DATETIME NOT NULL
+	, hora_fin DATETIME NOT NULL
+	, ubicacion NVARCHAR(60) NOT NULL
+	, cupo_max INT CONSTRAINT CK_cupo_max_val CHECK(cupo_max > 0)
+)
 GO
