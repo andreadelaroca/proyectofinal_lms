baf7f3a65ef7d3e471511019d2a403f89a2674f6
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-02 08:44:06 -0600

    fix(): Corrección de claves foráneas para que se considerara su schema

--- lms.sql
+++ lms.sql
@@ -31,8 +31,8 @@ GO
 --Tabla 4.1: Sesiones
 CREATE TABLE GestionEventos.Sesiones (
 	id_sesion INT IDENTITY(1,1) CONSTRAINT PK_id_sesion PRIMARY KEY
-	, id_tutor INT CONSTRAINT FK_id_tutor FOREIGN KEY REFERENCES Tutores(id_tutor)
-	, id_materia INT CONSTRAINT FK_id_materia FOREIGN KEY REFERENCES Materias(id_materia)
+	, id_tutor INT CONSTRAINT FK_id_tutor FOREIGN KEY REFERENCES GestionTutores.Tutores(id_tutor)
+	, id_materia INT CONSTRAINT FK_id_materia FOREIGN KEY REFERENCES GestionIdentidadCad.Materias(id_materia)
 	, fecha DATE NOT NULL
 	, hora_inicio DATETIME NOT NULL
 	, hora_fin DATETIME NOT NULL
@@ -47,8 +47,8 @@ GO
 --Tabla 4.2: Inscripciones
 CREATE TABLE GestionEventos.Inscripciones (
 	id_inscripcion INT IDENTITY(1,1) CONSTRAINT PK_id_inscripcion PRIMARY KEY
-	, id_estudiante INT CONSTRAINT FK_id_estudiante FOREIGN KEY REFERENCES Tutores(id_tutor)
-	, id_sesion INT CONSTRAINT FK_id_sesion FOREIGN KEY REFERENCES Sesiones(id_sesion)
+	, id_estudiante INT CONSTRAINT FK_id_estudiante FOREIGN KEY REFERENCES GestionTutores.Tutores(id_tutor)
+	, id_sesion INT CONSTRAINT FK_id_sesion FOREIGN KEY REFERENCES GestionEventos.Sesiones(id_sesion)
 	, fecha_inscripcion DATE CONSTRAINT DF_fecha_inscripcion DEFAULT GETDATE()
 	, estado BIT CONSTRAINT DF_estado DEFAULT 0
 	, created_at DATETIME CONSTRAINT DF_created_at_ses DEFAULT GETDATE()
@@ -60,7 +60,7 @@ GO
 --Tabla 4.3: Asistencias
 CREATE TABLE GestionEventos.Asistencias (
 	id_asistencia INT IDENTITY(1,1) CONSTRAINT PK_id_inscripcion PRIMARY KEY
-	, id_inscripcion INT CONSTRAINT FK_id_inscripcion FOREIGN KEY REFERENCES Inscripciones(id_inscripcion)
+	, id_inscripcion INT CONSTRAINT FK_id_inscripcion FOREIGN KEY REFERENCES GestionEventos.Inscripciones(id_inscripcion)
 	, estatus_asistencia NVARCHAR(30) CONSTRAINT CK_estatus_asistencia CHECK(estatus_asistencia IN ('Presente', 'Tarde', 'Falta justificada', 'Falta injustificada'))
 	, participacion TINYINT CONSTRAINT CK_participacion CHECK(participacion BETWEEN 0 AND 5)
 	, fecha_registro DATE NOT NULL
@@ -73,7 +73,7 @@ GO
 --Tabla 4.4: FeedbackEvaluaciones
 CREATE TABLE GestionEventos.FeedbackEvaluaciones (
 	id_feedback INT IDENTITY(1,1) CONSTRAINT PK_id_feedback PRIMARY KEY
-	, id_inscripcion INT FOREIGN KEY REFERENCES Inscripciones(id_inscripcion)
+	, id_inscripcion INT FOREIGN KEY REFERENCES GestionEventos.Inscripciones(id_inscripcion)
 	, comentario NVARCHAR(255) NOT NULL
 	, puntuacion_tutor TINYINT CONSTRAINT CK_participacion_tutor CHECK(puntuacion_tutor BETWEEN 0 AND 5)
 )
