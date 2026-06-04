13e856e9fee91ae47385826d4c8b3e4bd897f656
sar24001 <168153074+sar24001@users.noreply.github.com>
2026-06-03 21:55:21 -0600

    Inserts de la tabla 11-15

--- insercioneslmsDB.sql
+++ insercioneslmsDB.sql
@@ -83,10 +83,56 @@ VALUES
 (2,4.2,'2026-06-07 16:00:00',NULL),
 (3,3.8,'2026-06-08 17:00:00',NULL)
 GO
-INSERT INTO METRICAS_APRENDIZAJE (periodo,id_mat,estado_sesion,total_inscritos,total_asistencias,promedio_puntuacion,created_at,updated_at)
-VALUES
+    
+INSERT INTO METRICAS_APRENDIZAJE (periodo,id_mat,estado_sesion,total_inscritos,total_asistencias,promedio_puntuacion,created_at,updated_at) VALUES
 ('Primer semestre 2026',1,'Finalizada',20,18,4.7,'2026-06-10 08:00:00',NULL),
 ('Primer semestre 2026',2,'Activa',15,12,4.3,'2026-06-11 09:00:00','2026-06-11 09:30:00'),
 ('Segundo semestre 2026',3,'Finalizada',25,22,4.6,'2026-06-12 10:00:00',NULL),
 ('Segundo semestre 2026',1,'Cancelada',8,0,NULL,'2026-06-13 11:00:00',NULL)
 GO
+
+--Inserciones de la tabla Horarios del tutor
+INSERT INTO GestionTutores.HorariosTutor (id_tutor, dia_semana, hora_inicio, hora_fin) VALUES
+    (1, 1, '08:00', '12:00'),
+    (1, 3, '14:00', '18:00'),
+    (2, 2, '09:00', '11:00'),
+    (2, 5, '10:00', '13:00'),
+    (3, 4, '15:00', '19:00');
+GO
+
+--Inserciones de la tabla que vincula al tutor con la materia
+INSERT INTO GestionTutores.TutorMateria (id_tutor, id_materia) VALUES
+    (1, 101),
+    (1, 102),
+    (2, 103),
+    (3, 104),
+    (3, 105);
+GO
+
+--Inserciones de la tabla de si un tutor esta convalidado o no
+INSERT INTO GestionTutores.TutoresValidacion (id_tutor, is_valid) VALUES
+    (1, 1),
+    (2, 0),
+    (3, 1),
+    (4, 0),
+    (5, 1);
+GO
+
+--Inserciones de la tabla donde se guardan las acreditaciones academicas de los tutores
+INSERT INTO GestionTutores.Acreditaciones (id_tutor, archivo, file_name) VALUES
+    (1, 0x255044462D312E, 'certificado_mat.pdf'),
+    (2, 0xFFD8FFE000104A, 'titulo_prog.jpg'),
+    (3, 0x255044462D312E, 'diploma_redes.pdf'),
+    (4, 0xFFD8FFE000104A, 'certificado_bd.jpg'),
+    (5, 0x255044462D312E, 'curso_excel.pdf');
+GO
+
+--Inserciones de la tabla que contiene el perfil de los tutores
+INSERT INTO GestionTutores.PerfilTutor (id_tutor, descripcion) VALUES
+    (1, 'Tutor especializado en Matemáticas y Física, experiencia universitaria.'),
+    (2, 'Tutor de Programación en C# y Java, orientado a proyectos prácticos.'),
+    (3, 'Tutor en Redes Cisco, con certificación CCNA.'),
+    (4, 'Tutor en Bases de Datos SQL Server y Oracle.'),
+    (5, 'Tutor en Ofimática avanzada, Excel y Power BI.');
+GO
+
